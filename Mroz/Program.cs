d02de36569da6e79dca038b6e3309e1a2c082492
m1haww <ozunmihai5@gmail.com>
2026-05-04 18:02:33 +0300

    adaugat controllers

--- Mroz/Program.cs
+++ Mroz/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -8,6 +9,5 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
 app.Run();
