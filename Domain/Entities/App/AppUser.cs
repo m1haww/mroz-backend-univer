731f0d4abf7687a97e335e0520044dff20d63d89
m1haww <ozunmihai5@gmail.com>
2026-05-04 18:52:24 +0300

    adaugat app appuser si applesearchads credential

---
+++ Domain/Entities/App/AppUser.cs
@@ -0,0 +1,13 @@
+namespace Domain.Entities.App;
+
+public class AppUser
+{
+    public Guid Id { get; set; }
+    public string AppId { get; set; } = string.Empty;
+    public double TotalRevenue { get; set; }
+    public DateTime InstallDate { get; set; }
+    public long? CampaignId { get; set; }
+    public long? KeywordId { get; set; }
+    public long? AdGroupId { get; set; }
+    public string CountryCode { get; set; } = string.Empty;
+}
